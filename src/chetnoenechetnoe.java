71aa8fe42d965dd1ef25ff0d6ffe436c1a7ea0cc
Test1 <test1@Test1s-MBP.fritz.box>
2022-11-06 03:45:29 +0100

    updated

--- src/chetnoenechetnoe.java
+++ src/chetnoenechetnoe.java
@@ -5,6 +5,7 @@ public class chetnoenechetnoe {
 
         int value=6;
         boolean status = evenNumber(value);
+        //boolean status = evenNumberSwitchCase(status);
         if (status == true) {
             System.out.println("Число "+value+" - четное");
         }else {
@@ -19,6 +20,19 @@ public class chetnoenechetnoe {
         }
         else {
             status = false;
+        }
+        return status;
+    }
+
+    public static boolean evenNumberSwitchCase(int value) {
+        boolean status = false;
+        switch (value%2) {
+            case 0:
+                status = true;
+                break;
+            case 1:
+                status = false;
+                break;
         }
         return status;
     }
