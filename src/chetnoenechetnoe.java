ab3db7045a887deccd2b66994b670732a6449553
Test1 <test1@Test1s-MBP.fritz.box>
2022-11-06 03:31:49 +0100

    haha

---
+++ src/chetnoenechetnoe.java
@@ -0,0 +1,26 @@
+package Hausaufgaben;
+
+public class chetnoenechetnoe {
+    public static void main(String[] args){
+
+        int value=6;
+        boolean status = evenNumber(value);
+        if (status == true) {
+            System.out.println("Число "+value+" - четное");
+        }else {
+            System.out.println("Число "+value+" - нечетное");
+        }
+
+    }
+    public static boolean evenNumber(int value) {
+        boolean status;
+        if(value%2==0){
+            status = true;
+        }
+        else {
+            status = false;
+        }
+        return status;
+    }
+}
+
